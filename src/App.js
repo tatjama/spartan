1053aca640184bcdd8363a771f242050b12e81e1
Tanja <tanja120a@gmail.com>
2023-03-04 20:08:47 +0100

    S-7 styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 //Pages
+import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
 import NoMatch from './pages/NoMatch';
 
 const App =  () => {
   return (
     <Routes>
-      <Route path = "/" element = {<SearchPage/>}/>
+      <Route path = "/" element = {<HomePage/>}/>
+      <Route path = "/search" element = {<SearchPage/>}/>
+      <Route path = "/search/:id" element = {<SearchPage/>}/>
       <Route path ='*' element = {<NoMatch/>}/>
     </Routes>  
   );
