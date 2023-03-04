e49999aa3b3d82b4b0500231fec503eca37868a5
Tanja <tanja120a@gmail.com>
2023-03-04 20:08:17 +0100

    S-6 create pages

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,9 +1,18 @@
+//styles
+import { LoadingStyles } from '../components/loaders/LoadingStyles';
+//components
+import  Commenting  from '../components/loaders/Commenting';
+
 const NoMatch = () => {
-  return(
-    <div>
-      No Match
-    </div>
-  )
-};
+    return(
+        <LoadingStyles>
+          <Commenting/>
+          <div>
+            <h2>oops!</h2>
+            <p>the page doesn't seem to exist </p>
+          </div>
+        </LoadingStyles>
+    )
+}
 
 export default NoMatch;
