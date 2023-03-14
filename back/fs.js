817da462f2d5b4af4f8b27d1f7cf3e4ad22af5cf
Tanja <tanja120a@gmail.com>
2023-03-14 23:39:19 +0100

    S-15 - Back - Get data from API using https

--- back/fs.js
+++ back/fs.js
@@ -53,3 +53,32 @@ exec(`cd ${directoire} && dir`, (err, stdout, stderr) => {
 /*=====  End of Using exec from child_process for listing files  ======*/
 
 
+/*=============================================
+=            HTTP module GET                  =
+=============================================*/
+
+const https = require('https');
+
+https.get('https://reqres.in/api/users?page=2', (response) => {
+  let data = '';
+
+  response.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  response.on('end', () => {
+    const users = JSON.parse(data).data;
+    users.forEach(user => {
+      console.log(`${user.first_name}, ${user.last_name}`);
+    });
+  });
+
+}).on("error", (error) => {
+  console.error(error);
+});
+
+/*=====  End of HTTP module GET        ======*/
+
+
+
+
